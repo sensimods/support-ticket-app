5e8246054885379645f09e2ffdda17be4c8832b5
Andy Chantler <munchermods@gmail.com>
2022-12-27 10:48:28 +0100

    Basic backend and frontend all setup and working

---
+++ backend/models/noteModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+// mongoose.set('strictQuery', false)
+
+const noteSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Ticket',
+    },
+    text: {
+      type: String,
+      required: [true, 'Please add some text'],
+    },
+    isStaff: {
+      type: Boolean,
+      default: false
+    },
+    staffId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Note', noteSchema)
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,10 +3,23 @@ const router = express.Router()
 const {protect} = require('../middleware/authMiddleware')
 const {
   getTickets,
+  getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketController')
 
+// Re-route into note router
+const noteRouter = require('../routes/noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+router
+.route('/:id')
+.get(protect, getTicket)
+.delete(protect, deleteTicket)
+.put(protect, updateTicket)
+
 
 module.exports = router
